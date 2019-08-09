7a6caca57729a5af4354dc33d166f1a289014f38
ashishpratheesh <ashishpradeesh@gmail.com>
2019-08-09 19:32:08 -0400

    Updated Data Item view

--- app/src/main/java/com/example/c0754254_mad3125_midterm/HomeActivity.java
+++ app/src/main/java/com/example/c0754254_mad3125_midterm/HomeActivity.java
@@ -116,7 +116,6 @@ public class HomeActivity extends AppCompatActivity {
 
         return new DataModel(flight_number,launch_year,image_URL);
     }
-    //yyyy
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
